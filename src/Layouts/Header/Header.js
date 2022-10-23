a913448861c92f1e7a65060a4eec82912cc9ea94
jahupe <jahupe@gmail.com>
2022-10-23 01:17:53 -0500

    Modification 23102022

---
+++ src/Layouts/Header/Header.js
@@ -0,0 +1,60 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Image, Nav, Navbar, Row } from "react-bootstrap";
+import { Binoculars, Display } from "react-bootstrap-icons";
+import logo from "../../Assets/Images/logo-small.png";
+import "./Header.css";
+
+function Header() {
+  return (
+    <Container fluid>
+      <Row>
+        <Navbar
+          bg="light"
+          expand="lg"
+          style={{
+            paddingTop: "0px",
+            paddingBottom: "0px",
+            paddingLeft: "0px",
+            paddingRight: "0px",
+            backgroundColor: "#FFFFFF",
+          }}
+        >
+          <Image
+            src={logo}
+            className="navbar-logo"
+            style={{
+              marginLeft: "15px",
+            }}
+          />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="justify-content-end" style={{ width: "100%" }}>
+              <Nav.Link
+                href="#home"
+                style={{ color: "#e70031", fontWeight: "bold" }}
+              >
+                Explora
+              </Nav.Link>
+              <Nav.Link href="#link">Biblioteca</Nav.Link>
+              <Nav.Link href="#link">Contactanos</Nav.Link>
+              <Nav.Link href="#link">Aula Virtual</Nav.Link>
+              <div
+                className="intranet-button"
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  paddingLeft: "70px",
+                }}
+              >
+                <Display color="white" size={22} className="align-top" />
+                Intranet para alumnos
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Row>
+    </Container>
+  );
+}
+
+export default Header;
