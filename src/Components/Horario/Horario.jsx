75e2ff77799ccbda811aac5f214fbdf3be5dd6fc
Alex Abad Pelaez <abad@rashperu.com>
2023-08-10 23:48:31 -0500

    actualizacion de imagenes en horario

--- src/Components/Horario/Horario.jsx
+++ src/Components/Horario/Horario.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // import { Modal } from "react-bootstrap";
 import Modal from "@mui/material/Modal";
 import {
@@ -11,6 +11,8 @@ import "./Horario.css";
 import { Box, Button, Container } from "@mui/material";
 
 const Horario = (props) => {
+  const [currentImage, setCurrentImage] = useState();
+
   const schedule_Images = props.ciclo.schedule_Image;
   const images_cant = schedule_Images.length;
   const image_name = schedule_Images[0].url;
@@ -32,7 +34,56 @@ const Horario = (props) => {
     }
   };
 
+  console.log(file_name, "file_name");
+
   const imagePath = `../src/Components/Horario/Images/Schedule/${file_name}.png`;
+
+  useEffect(() => {
+    file_name === "sasm" &&
+      setCurrentImage(
+        "https://firebasestorage.googleapis.com/v0/b/grupo-ciencias.appspot.com/o/Landing%20GC%2FNuestros%20Ciclos%2FHorarios%2Fsasm.png?alt=media&token=4964168d-a8f3-4119-9454-ddacdb905f88"
+      );
+    file_name === "srsm_1" &&
+      setCurrentImage(
+        "https://firebasestorage.googleapis.com/v0/b/grupo-ciencias.appspot.com/o/Landing%20GC%2FNuestros%20Ciclos%2FHorarios%2Fsrsm_1.png?alt=media&token=d1943b9c-1963-42c9-8143-874acdfe715f"
+      );
+    file_name === "srsm_2" &&
+      setCurrentImage(
+        "https://firebasestorage.googleapis.com/v0/b/grupo-ciencias.appspot.com/o/Landing%20GC%2FNuestros%20Ciclos%2FHorarios%2Fsrsm_2.png?alt=media&token=c17b74f4-73a2-460f-ac1b-9b11bc06cc9f"
+      );
+    file_name === "sruni" &&
+      setCurrentImage(
+        "https://firebasestorage.googleapis.com/v0/b/grupo-ciencias.appspot.com/o/Landing%20GC%2FNuestros%20Ciclos%2FHorarios%2Fsruni.png?alt=media&token=f5ee0819-c11a-43de-bf2a-f6e55a0a8740"
+      );
+    file_name === "ssasm" &&
+      setCurrentImage(
+        "https://firebasestorage.googleapis.com/v0/b/grupo-ciencias.appspot.com/o/Landing%20GC%2FNuestros%20Ciclos%2FHorarios%2Fssasm.png?alt=media&token=88bdd6e0-06b4-4371-a8fd-429366514c2d"
+      );
+    file_name === "ssbuni" &&
+      setCurrentImage(
+        "https://firebasestorage.googleapis.com/v0/b/grupo-ciencias.appspot.com/o/Landing%20GC%2FNuestros%20Ciclos%2FHorarios%2Fssbuni.png?alt=media&token=334f77c1-2ba7-4ed9-b2f9-3e0ee3c51497"
+      );
+    file_name === "ssiuni" &&
+      setCurrentImage(
+        "https://firebasestorage.googleapis.com/v0/b/grupo-ciencias.appspot.com/o/Landing%20GC%2FNuestros%20Ciclos%2FHorarios%2Fssiuni.png?alt=media&token=74132f59-d9c0-446c-af30-fa830144465d"
+      );
+    file_name === "sssm_1" &&
+      setCurrentImage(
+        "https://firebasestorage.googleapis.com/v0/b/grupo-ciencias.appspot.com/o/Landing%20GC%2FNuestros%20Ciclos%2FHorarios%2Fsssm_1.png?alt=media&token=b55716ba-4395-43c8-aa9d-f98fec8cfc08"
+      );
+    file_name === "sssm_2" &&
+      setCurrentImage(
+        "https://firebasestorage.googleapis.com/v0/b/grupo-ciencias.appspot.com/o/Landing%20GC%2FNuestros%20Ciclos%2FHorarios%2Fsssm_2.png?alt=media&token=a05d22d0-0288-4572-b2c3-c42006e591da"
+      );
+    file_name === "svsm" &&
+      setCurrentImage(
+        "https://firebasestorage.googleapis.com/v0/b/grupo-ciencias.appspot.com/o/Landing%20GC%2FNuestros%20Ciclos%2FHorarios%2Fsvsm.png?alt=media&token=60e04938-a82f-43be-b171-f714a59cdb2e"
+      );
+    file_name === "svuni" &&
+      setCurrentImage(
+        "https://firebasestorage.googleapis.com/v0/b/grupo-ciencias.appspot.com/o/Landing%20GC%2FNuestros%20Ciclos%2FHorarios%2Fsvuni.png?alt=media&token=0fe549da-71f3-40f6-a0f4-be02139f0788"
+      );
+  }, [file_name]);
 
   return (
     <>
@@ -99,7 +150,8 @@ const Horario = (props) => {
               </>
             )}
             <Box sx={{ display: { xs: "block", sm: "block" } }}>
-              <img width="100%" src={imagePath} alt="Imagen de modal" />
+              {/* <img width="100%" src={imagePath} alt="Imagen de modal" /> */}
+              <img width="100%" src={currentImage} alt="Imagen de modal" />
             </Box>
           </Box>
         </Modal>
